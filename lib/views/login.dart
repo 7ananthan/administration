0227f941bd6a9bb3b45396b8075477d20d4d08a6
7ananthan <7ananthan@gmail.com>
2020-04-26 23:44:00 +0530

    nothing changed

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -8,48 +8,48 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
-          top: true,
-          bottom: true,
-          child: ListView(
-            children: <Widget>[
-              TextField(
-                controller: user,
-                decoration: InputDecoration(hintText: "username"),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              TextField(
-                obscureText: true,
-                controller: pass,
-                decoration: InputDecoration(hintText: "password"),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              RaisedButton(
-                child: Text("LOGIN"),
-                onPressed: () {
-                  String getuser = user.text;
-                  String getpass = pass.text;
-                  if ((getuser == "admin") && (getpass == "12345"))
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Window()));
-                  else
-                    print("error");
-                },
-              ),
-              SizedBox(height: 10.0),
-              RaisedButton(
-                child: Text("SIGN UP"),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Signup()));
-                },
-              )
-            ],
+    return SafeArea(
+      top: true,
+      bottom: true,
+      child: ListView(
+        children: <Widget>[
+          TextField(
+            controller: user,
+            decoration: InputDecoration(hintText: "username"),
           ),
+          SizedBox(
+            height: 10.0,
+          ),
+          TextField(
+            obscureText: true,
+            controller: pass,
+            decoration: InputDecoration(hintText: "password"),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          RaisedButton(
+            child: Text("LOGIN"),
+            onPressed: () {
+              String getuser = user.text;
+              String getpass = pass.text;
+              if ((getuser == "admin") && (getpass == "12345"))
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Window()));
+              else
+                print("error");
+            },
+          ),
+          SizedBox(height: 10.0),
+          RaisedButton(
+            child: Text("SIGN UP"),
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Signup()));
+            },
+          )
+        ],
+      ),
     );
   }
 }
